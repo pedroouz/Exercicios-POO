0233796a0c91bfd643c77bfa9be7e23bfcb4afc1
pedroouz <andradepedro2106@gmail.com>
2026-03-06 00:11:19 -0300

    Segundo commit

---
+++ Roteiro 2/Ex001/Program.cs
@@ -0,0 +1,70 @@
+﻿/*Exercício 1: Criando uma Classe Produto
+Crie uma classe Produto que tenha os seguintes atributos privados:
+● nome (string)
+● preco (decimal)
+Use propriedades (get e set) para:
+✔ Permitir a leitura do nome.
+✔ Garantir que o preço nunca seja negativo.
+Requisitos:
+1. Criar um construtor que recebe o nome e o preço do produto.
+2. Criar um método ExibirDetalhes() que imprime nome e preço.*/
+
+class Produto
+{
+    private string _nome;
+    private decimal _preco;
+
+    public Produto(string nome, decimal preco)
+    {
+        _nome = nome;
+        if (preco >= 0)
+        {
+            _preco = preco;
+        }
+        else
+        {
+            _preco = 0;
+        }
+    }
+
+    public void MostrarDetalhes()
+    {
+        Console.WriteLine($"Nome do produto: {_nome}\nPreco: {_preco}");
+    }
+
+    public string Nome
+    {
+        get
+        {
+            return _nome;
+        }
+    }
+
+    public decimal Preco
+    {
+        get
+        {
+            return _preco;
+        }
+        set
+        {
+            if (value >= 0)
+            {
+                _preco = value;
+            }
+            else
+            {
+                Console.WriteLine("Erro!");
+            }
+        }
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Produto p1 = new Produto("Fone de ouvido", -200);
+        p1.MostrarDetalhes();
+    }
+}
---
+++ Roteiro 2/Ex002/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Exercício 2: Classe Carro com Controle de Velocidade
+Crie uma classe Carro com os seguintes atributos privados:
+● modelo (string)
+● velocidadeAtual (int)
+Use propriedades (get e set) para:
+✔ A velocidadeAtual nunca pode ser menor que 0.
+✔ Criar métodos Acelerar(int valor) e Frear(int valor) para modificar a
+velocidade.
+*/
+
+public class Carro
+{
+    private string _modelo;
+    private int _velocidadeAtual;
+
+    public Carro(string modelo)
+    {
+        _modelo = modelo;
+    }
+
+    public int VelocidadeAtual
+    {
+        get{return _velocidadeAtual;}
+        private set
+        {
+            if(value >= 0)
+            {
+                _velocidadeAtual = value;
+            }
+            else
+            {
+                _velocidadeAtual = 0;
+            }
+        }
+    }
+
+
+}
